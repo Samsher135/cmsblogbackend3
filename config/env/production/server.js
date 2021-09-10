6a2ee819ec03dc15dc3f141a7f4f32d7f75c4ecd
Samsher135 <12345samsingh.samu@gmail.com>
2021-09-10 13:40:24 +0530

    Update database config

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
-    url: env('postgres://pctbblvqoxjhpa:05430ad5040e1cfd5c072b0ac40145c933f2fc52af4865bae71a319567548503@ec2-54-145-188-92.compute-1.amazonaws.com:5432/d9j89jgv332deh'),
+    url: env('https://cmsblogbackend.herokuapp.com'),
   });
   
